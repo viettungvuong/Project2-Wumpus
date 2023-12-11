d3414d0be3996dbd813cea9670fc24d50e406845
viettungvuong <viettungvuong@gmail.com>
2023-12-11 16:47:27 +0700

    Add MP, MT, resolution

---
+++ logic.py
@@ -0,0 +1,18 @@
+from sympy import Not, Or
+
+def modus_ponens(rule, antecedent):
+    if antecedent in rule.args:
+        return rule.rhs
+
+def modus_tollens(rule, negated_consequent):
+    if Not(rule.rhs) == negated_consequent:
+        return Not(rule.lhs)
+
+def resolution_refutation(clause1, clause2):
+    resolved = Or()
+
+    for literal in clause1.args:
+        if Not(literal) in clause2.args: # if the negation of the literal is in the second clause
+            resolved = Or(resolved, Or([other for other in clause1.args if other != literal] + [other for other in clause2.args if other != Not(literal)]))
+
+    return resolved
