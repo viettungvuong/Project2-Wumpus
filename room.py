9eefe973fffb963f929aeee0cae36dc10eab8c4a
viettungvuong <viettungvuong@gmail.com>
2023-12-11 16:39:02 +0700

    Change logic in class

--- room.py
+++ room.py
@@ -1,3 +1,6 @@
+from sympy import symbols, Function, satisfiable
+from sympy.logic.boolalg import And, Implies, Not, Or
+
 rooms = set()
 
 class Room:
@@ -5,30 +8,47 @@ class Room:
         self.x = x
         self.y = y
 
-        self.wumpus = False
-        self.gold = False
-        self.pit = False
-        self.breeze = False
-        self.stench = False
-        self.bat = False
+        self.wumpus = None
+        self.gold = None
+        self.pit = None
+        self.breeze = None
+        self.stench = None
 
-        self.agent = False
+        self.agent = None # agent currently here
 
         self.visited = False
 
+        self.surrounding_rooms = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+
     def set_room(self, str):
         if str.__contains__("W"):
-            self.wumpus = True
+            self.wumpus = symbols(f"Wumpus({self.x}, {self.y})")
         if str.__contains__("G"):
-            self.gold = True
+            self.gold = symbols(f"Gold({self.x}, {self.y})")
         if str.__contains__("P"):
-            self.pit = True
+            self.pit = symbols(f"Pit({self.x}, {self.y})")
         if str.__contains__("B"):
-            self.breeze = True
+            self.breeze = symbols(f"Breeze({self.x}, {self.y})")
         if str.__contains__("S"):
-            self.stench = True
+            self.stench = symbols(f"Stench({self.x}, {self.y})")
         if str.__contains__("A"):
-            self.agent = True
+            self.agent = symbols(f"Agent({self.x}, {self.y})")
 
     def wumpus_hit(self):
-        self.wumpus = False
+        self.wumpus = Not(self.wumpus)
+
+    def possible_wumpus(self):
+        surrounding_wumpus = [symbols(f"Wumpus({r[0]}, {r[1]})") for r in self.surrounding_rooms]
+        return Implies(self.stench, Or(*surrounding_wumpus))
+
+    def possible_pit(self):
+        surrounding_pit = [symbols(f"Pit({r[0]}, {r[1]})") for r in self.surrounding_rooms]
+        return Implies(self.breeze, Or(*surrounding_pit))
+
+    def has_wumpus(self):
+        surrounding_stench = [symbols(f"Stench({r[0]}, {r[1]})") for r in self.surrounding_rooms]
+        return Implies(self.wumpus, And(*surrounding_stench))
+
+    def has_pit(self):
+        surrounding_breeze = [symbols(f"Breeze({r[0]}, {r[1]})") for r in self.surrounding_rooms]
+        return Implies(self.wumpus, And(*surrounding_breeze))
